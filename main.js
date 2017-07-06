7235bd1f4be88bb02c2be4b16c9dcd7893df36e4
Grant McGrath <grantmcgrath@me.com>
2017-07-06 11:45:04 -0400

    The only comment I can add that would not get me fired in a job would be, 'done'.

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 // Rock Paper Scissors Function
-function rps () {
+function rps (player1, player2) {
   // write code here
+  if (player1 === player2) {
+    return "Tie!!!";
+  } else if (player1 === "rock") {
+    if (player2 === "scissors"){
+      return"rock Wins!";
+    } else {
+      return "paper Wins!";
+    }
+  } else if (player1 === "paper") {
+    if (player2 === "rock") {
+      return "paper Wins!";
+    } else {
+      return "scissors Wins!";
+    }
+  } else if (player1 === "scissors") {
+    if (player2 === "rock") {
+      return "rock Wins!";
+    } else {
+      return "scissors Wins!";
+    }
+  }
 }
 
 
